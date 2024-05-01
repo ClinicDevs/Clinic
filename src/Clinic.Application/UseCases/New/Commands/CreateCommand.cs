5ccd06c5fa7a4b9f8cc1ab2377cc4af3422e2de6
Akmalxo'ja <145533924+akmalxoja@users.noreply.github.com>
2024-05-01 16:30:06 +0500

    Add CreateCommands

---
+++ src/Clinic.Application/UseCases/New/Commands/CreateCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Application.UseCases.New.Commands
+{
+    public class CreateCommand
+    {
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Date { get; set; }
+    }
+}
---
+++ src/Clinic.Application/UseCases/Skill/Commands/CreateCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Application.UseCases.Skill.Commands
+{
+    public class CreateCommand
+    {
+        public string Name { get; set; }
+    }
+}
