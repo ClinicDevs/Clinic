f268f51762684819ed08b6b445d9265612ab0c62
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-01 15:24:12 +0500

    Add-User-Model

---
+++ src/Clinic.Domain/Entities/Auth/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinic.Domain.Entities.Auth
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Firsname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
