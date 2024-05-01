6faf97c9a13fc64dca832a04057251e0a0e11f97
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-01 15:16:41 +0500

    add feedbackmodel

---
+++ src/Clinic.Domain/Entities/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Domain.Entities
+{
+    public class Feedback
+    {
+        public Guid Id { get; set; }
+        public string? VideoPath { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset WritedDate { get; set; }
+    }
+}
