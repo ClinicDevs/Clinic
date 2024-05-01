ce3341f21410d1ecebe905571c54e7a48a6e042d
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-01 15:49:32 +0500

    Add-Abstraction

---
+++ src/Clinic.Application/Abstractions/IClinincDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Clinic.Domain.Entities;
+using Clinic.Domain.Entities.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Application.Abstractions
+{
+    public interface IClinincDbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Diplom> Diploms { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<New> News { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Specialist> Specialists { get; set; }
+    }
+}
