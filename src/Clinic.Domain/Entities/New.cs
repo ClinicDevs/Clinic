131b2965a0065a5059ffa72b73872e9cb6ba6b6e
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-01 15:09:31 +0500

    add new models

---
+++ src/Clinic.Domain/Entities/New.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Domain.Entities
+{
+    public class New
+    {
+        public Guid Id { get; set; }
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        
+    }
+}
