2fbc4b3b5a6d6bd1c0ec7419886cabad4d923501
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-02 10:40:18 +0500

    Add Fedback crud

--- src/Clinic.Application/UseCases/Feedbacks/Handlers/QueryHandlers/GetAllFeedbacksQueryHandler.cs
+++ src/Clinic.Application/UseCases/Feedbacks/Handlers/QueryHandlers/GetAllFeedbacksQueryHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Clinic.Application.Abstractions;
+using Clinic.Application.UseCases.Feedbacks.Queries;
+using Clinic.Domain.DTOs;
+using Clinic.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,22 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Feedbacks.Handlers.QueryHandlers
 {
-    public class GetAllFeedbacksQueryHandler
+    public class GetAllFeedbacksQueryHandler : IRequestHandler<GetAllFeedbacksQuery, IEnumerable<Feedback>>
     {
+        private readonly IClinincDbContext _clinicDbContext;
+
+        public GetAllFeedbacksQueryHandler(IClinincDbContext clinincDbContext)
+        {
+            _clinicDbContext = clinincDbContext;
+        }
+
+        public async Task<IEnumerable<Feedback>> Handle(GetAllFeedbacksQuery request, CancellationToken cancellationToken)
+        {
+            return await _clinicDbContext.Feedbacks
+                    .Skip(request.PageIndex)
+                        .Take(request.Size)
+                            .Where(x => x.IsDeleted == false)
+                                .ToListAsync(cancellationToken);
+        }
     }
 }
--- src/Clinic.Application/UseCases/Feedbacks/Queries/GetAllFeedbacksQuery.cs
+++ src/Clinic.Application/UseCases/Feedbacks/Queries/GetAllFeedbacksQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clinic.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Feedbacks.Queries
 {
-    public class GetAllFeedbacksQuery
+    public class GetAllFeedbacksQuery : IRequest<IEnumerable<Feedback>>
     {
+        public int PageIndex { get; set; }
+        public int Size { get; set; }
     }
 }
