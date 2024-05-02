3c979a4bf5df7d5548c108e421d2c973d4893e82
Abu_Programmiy <abdukholiq0907@gmail.com>
2024-05-02 10:07:30 +0500

    Add-New-CRUD

--- src/Clinic.Application/UseCases/News/Handlers/QueryHandlers/GetNewsByIdQueryHandler.cs
+++ src/Clinic.Application/UseCases/News/Handlers/QueryHandlers/GetNewsByIdQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Clinic.Application.Abstractions;
+using Clinic.Application.UseCases.News.Queries;
+using Clinic.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.Application.UseCases.News.Handlers.QueryHandlers
 {
-    public class GetNewsByIdQueryHandler
+    public class GetNewsByIdQueryHandler : IRequestHandler<GetNewsByIdQuery, New>
     {
+        private readonly IClinincDbContext _clinincDbContext;
+
+        public GetNewsByIdQueryHandler(IClinincDbContext clinincDbContext)
+        {
+            _clinincDbContext = clinincDbContext;
+        }
+
+        public async Task<New> Handle(GetNewsByIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _clinincDbContext.News.FirstOrDefaultAsync(n => n.Id == request.Id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Somethig went wrong");
+            }
+        }
     }
 }
--- src/Clinic.Application/UseCases/News/Queries/GetAllNewsQuery.cs
+++ src/Clinic.Application/UseCases/News/Queries/GetAllNewsQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clinic.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.News.Queries
 {
-    public class GetAllNewsQuery
+    public class GetAllNewsQuery:IRequest<IEnumerable<New>>
     {
+        public int PageIndex {  get; set; }
+        public int Size { get; set; }
     }
 }
