3c30cf12b0d0cd56e3951ad27bad42efaf3f957e
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-02 15:58:36 +0500

    Add-Result-Controller

--- src/Clinic.Application/UseCases/Results/Handlers/QueryHandlers/GetAllResultsQueryHandler.cs
+++ src/Clinic.Application/UseCases/Results/Handlers/QueryHandlers/GetAllResultsQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Clinic.Application.Abstractions;
+using Clinic.Application.UseCases.Results.Queries;
+using Clinic.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,26 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Results.Handlers.QueryHandlers
 {
-    internal class GetAllResultsQueryHandler
+    public class GetAllResultsQueryHandler : IRequestHandler<GetAllResultsQuery,IEnumerable<Result>>
     {
+        private readonly IClinincDbContext _clinincDbContext;
+
+        public GetAllResultsQueryHandler(IClinincDbContext clinincDbContext)
+        {
+            _clinincDbContext = clinincDbContext;
+        }
+
+        public async Task<IEnumerable<Result>> Handle(GetAllResultsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _clinincDbContext.Results.Where(r => r.IsDeleted == false).Skip(request.PageIndex - 1).Take(request.Size).ToListAsync();
+            }
+            catch
+            {
+                throw new Exception("Something Went Wrog from AkmalXo'ja");
+            }
+        }
+
     }
 }
--- src/Clinic.Application/UseCases/Results/Handlers/QueryHandlers/GetResultByIdQueryhandler.cs
+++ src/Clinic.Application/UseCases/Results/Handlers/QueryHandlers/GetResultByIdQueryhandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Clinic.Application.Abstractions;
+using Clinic.Application.UseCases.Results.Queries;
+using Clinic.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,25 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Results.Handlers.QueryHandlers
 {
-    internal class GetResultByIdQueryhandler
+    public class GetResultByIdQueryhandler:IRequestHandler<GetResultByIdQuery,Result>
     {
+        private readonly IClinincDbContext _clinincDbContext;
+
+        public GetResultByIdQueryhandler(IClinincDbContext clinincDbContext)
+        {
+            _clinincDbContext = clinincDbContext;
+        }
+
+        public async Task<Result> Handle(GetResultByIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await  _clinincDbContext.Results.Where(r => r.IsDeleted == false).FirstOrDefaultAsync(r => r.Id == request.Id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Somthing went wrong", ex);
+            }
+        }
     }
 }
