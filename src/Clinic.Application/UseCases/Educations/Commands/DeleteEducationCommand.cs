a8893dcc441740139b0693456074b3612ccdc890
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-02 09:13:07 +0500

    add Education CRUD

--- src/Clinic.Application/UseCases/Educations/Commands/DeleteEducationCommand.cs
+++ src/Clinic.Application/UseCases/Educations/Commands/DeleteEducationCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clinic.Domain.DTOs;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Educations.Commands
 {
-    public class DeleteEducationCommand
+    public class DeleteEducationCommand : IRequest<ResponseModel>
     {
+        public Guid Id { get; set; }
     }
 }
--- src/Clinic.Application/UseCases/Educations/Queries/GetAllEducationsQuery.cs
+++ src/Clinic.Application/UseCases/Educations/Queries/GetAllEducationsQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clinic.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Educations.Queries
 {
-    public class GetAllEducationsQuery
+    public class GetAllEducationsQuery : IRequest<IEnumerable<Education>>
     {
     }
 }
