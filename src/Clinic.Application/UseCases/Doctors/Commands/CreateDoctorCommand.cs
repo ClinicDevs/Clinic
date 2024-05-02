7b78277ae1aa65ed56f0495e143d3069a9807008
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-02 11:07:48 +0500

    Add Docotr create

--- src/Clinic.Application/UseCases/Doctors/Commands/CreateDoctorCommand.cs
+++ src/Clinic.Application/UseCases/Doctors/Commands/CreateDoctorCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Clinic.Domain.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,14 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.UseCases.Doctors.Commands
 {
-    public class CreateDoctorCommand
+    public class CreateDoctorCommand : IRequest<ResponseModel>
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int StartWork { get; set; }
+        public string TUsername { get; set; }
+        public IFormFile? Picture { get; set; }
+        public Guid SpecialistId { get; set; }
+        public Guid ServiceTypeId { get; set; }
     }
 }
