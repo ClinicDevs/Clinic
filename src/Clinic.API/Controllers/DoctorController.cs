7f5e5fd08f6658a711fc97bb3596ad07288d6831
Abu_Programmiy <abdukholiq23@gmail.com>
2024-05-03 08:16:44 +0500

    Add-Doctor-Controller

--- src/Clinic.API/Controllers/DoctorController.cs
+++ src/Clinic.API/Controllers/DoctorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Clinic.Application.UseCases.Doctors.Commands;
+using Clinic.Application.UseCases.Doctors.Queries;
+using Clinic.Domain.DTOs;
+using Clinic.Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Clinic.API.Controllers
@@ -7,5 +11,48 @@ namespace Clinic.API.Controllers
     [ApiController]
     public class DoctorController : ControllerBase
     {
+        private readonly IMediator _mediatr;
+
+        public DoctorController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpPost]
+        public async Task<ResponseModel> CreateDoctor(CreateDoctorCommand request)
+        {
+            return await _mediatr.Send(request);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Doctor>> GetAllDoctor(int pageIndex, int size)
+        {
+            return await _mediatr.Send(new GetAllDoctorsQuery
+            {
+                PageIndex = pageIndex,
+                Size = size
+            });
+        }
+
+        [HttpGet]
+        public async Task<Doctor> GetDoctorById(Guid Id)
+        {
+            return await _mediatr.Send(new GetDoctorByIdQuery
+            {
+                Id = Id
+            });
+        }
+
+        [HttpPut]
+        public async Task<ResponseModel> UpdateDoctor(UpdateDoctorCommand request)
+        {
+            return await _mediatr.Send(request);
+        }
+
+        [HttpDelete]
+        public async Task<ResponseModel> DeleteDoctor(DeleteDoctorCommand request)
+        {
+            return await _mediatr.Send(request);
+        }
     }
 }
